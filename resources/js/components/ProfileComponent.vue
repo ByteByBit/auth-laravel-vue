bb16da5f4c1b6c27ebbdb2d57c4b851b377e453b
rt0 <rajm.toth@gmail.com>
2021-03-29 20:00:46 +0200

    Initial commit.

---
+++ resources/js/components/ProfileComponent.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <div class="card">
+        <div class="card-header">Profile</div>
+        <div class="media" style="padding:5px;">
+            <div class="media-left media-top">
+                <img :src="src" :alt="name" class="media-object" style="width:60px;padding:5px;">
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading">{{ name }}</h4>
+                <p>Lorem ipsum...</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+
+        data() {
+            return {
+                name: '',
+                src: ''
+            }
+        },
+
+        mounted() {
+            axios.get('/api/users')
+                .then(response => {
+                this.name = response.data.name;
+                this.src = response.data.src;
+            });
+        }
+
+    }
+</script>
